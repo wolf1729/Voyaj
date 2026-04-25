aeab8029135d733fe7412c9f9c11d59f307d697f
wolf1729 <aryanm1729@gmail.com>
2026-04-25 16:41:21 +0530

    fix: suppress hydration warning from browser extensions
    - Added suppressHydrationWarning to root layout to prevent console errors caused by attribute-injecting browser extensions

--- src/app/layout.js
+++ src/app/layout.js
@@ -62,6 +62,7 @@ export default function RootLayout({ children }) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
+      suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col">
         <ClientProviders>
