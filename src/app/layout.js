fa4b9d18354cf44976794ce98f08833fe2558ad8
wolf1729 <aryanm1729@gmail.com>
2026-04-25 20:53:21 +0530

    chore: added vercel speed insights package

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { ClientProviders } from "@/components/providers/ClientProviders";
 import "./globals.css";
 
@@ -68,6 +69,7 @@ export default function RootLayout({ children }) {
         <ClientProviders>
           {children}
           <Analytics />
+          <SpeedInsights />
         </ClientProviders>
       </body>
     </html>
